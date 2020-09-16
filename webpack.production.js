fd22d93c6a745da796e378d13fd898ea5be745ff
Edson Alcala <edson_alcala@msn.com>
2020-09-16 14:28:13 +0100

    fix production issue and changed svg loader

--- webpack.production.js
+++ webpack.production.js
@@ -3,7 +3,11 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
     mode: 'production',
-    devtool: "source-map"
+    devtool: "source-map",
+    externals: [
+        'react',
+        'react-dom'
+    ]
 });
 
 
